c2a4314c6e6f8a0205951c1f5cd3f4b5d7e9c75c
yair.spielmann <yair.spielmann@synergy-learning.com>
2017-08-31 12:21:47 +0100

    MDL-59512 tool_oauth2: add option for basic authentication

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -2793,5 +2793,20 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2017102100.01);
     }
 
+    if ($oldversion < 2017102500.001) {
+
+        // Define field basicauth to be added to oauth2_issuer.
+        $table = new xmldb_table('oauth2_issuer');
+        $field = new xmldb_field('basicauth', XMLDB_TYPE_INTEGER, '2', null, XMLDB_NOTNULL, null, '0', 'showonloginpage');
+
+        // Conditionally launch add field basicauth.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2017102500.001);
+    }
+
     return true;
 }
